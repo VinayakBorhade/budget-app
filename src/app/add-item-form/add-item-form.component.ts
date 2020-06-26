238a9edfe0f6a437d4ee77f4a055d2c25b235767
VinayakBorhade <vinayakborhade@gmail.com>
2020-06-26 13:58:48 +0530

    getting started with form module

--- src/app/add-item-form/add-item-form.component.ts
+++ src/app/add-item-form/add-item-form.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { BudgetItem } from 'src/shared/models/budget-item.model';
 
 @Component({
-  selector: 'app-add-item-form',
-  templateUrl: './add-item-form.component.html',
-  styleUrls: ['./add-item-form.component.scss']
+    selector: 'app-add-item-form',
+    templateUrl: './add-item-form.component.html',
+    styleUrls: ['./add-item-form.component.scss']
 })
 export class AddItemFormComponent implements OnInit {
 
-  constructor() { }
+    @Input() item: BudgetItem = new BudgetItem('', null);
 
-  ngOnInit(): void {
-  }
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+    onSubmit(form: NgForm ) {
+        console.log(form);
+    }
 
 }
