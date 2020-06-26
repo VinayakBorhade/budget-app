f231d5b90c658348eb1db6d45dc9d56d1a73243e
VinayakBorhade <vinayakborhade@gmail.com>
2020-06-26 18:19:08 +0530

    form module done, rendering item components with ngFor directive

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { BudgetItem } from 'src/shared/models/budget-item.model';
 
 @Component({
-  selector: 'app-main-page',
-  templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.scss']
+    selector: 'app-main-page',
+    templateUrl: './main-page.component.html',
+    styleUrls: ['./main-page.component.scss']
 })
 export class MainPageComponent implements OnInit {
 
-  constructor() { }
+    budgetItems: BudgetItem[] = new Array<BudgetItem>();
 
-  ngOnInit(): void {
-  }
+    constructor () { }
 
+    ngOnInit() { }
+
+    addItem(newItem: BudgetItem) {
+        this.budgetItems.push(newItem);
+    }
 }
